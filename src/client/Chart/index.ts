1c52f094e7ae99476081e9da2aea470bd91d266b
Matt Palmer <matt@silvercreekit.com>
2016-08-06 18:01:43 -0400

    a

--- src/client/Chart/index.ts
+++ src/client/Chart/index.ts
@@ -1,29 +1,33 @@
 import * as d3 from "d3";
 
+interface Schema {
+    heading: String;
+}
+
+interface Plot {
+    color: String | null;
+}
+
+
 export default class Chart {
     
-    private elements: Array<Element>;
+    public elements: Array<Element>;
+    public schema: Schema;
+    public plot: Array<Plot>;
 
-    constructor(elements: string|Element|NodeList, schema: Object) {
+    constructor(elements: string | Element | NodeList, schema: Schema, plot: Array<Plot>) {
         this.elements = Chart.parseTarget(elements);
+        this.schema = schema;
+        this.plot = plot;
     }
 
     /**
      *
      */
-    private static parseTarget(elements: string|Element|NodeList): Array<Element> {
+    private static parseTarget(elements: string | Element | NodeList): Array<Element> {
         if (typeof elements === "string") return Array.from(document.querySelectorAll(elements));
         if (typeof elements === "NodeList") return Array.from(<NodeListOf<Element>>elements);
         if (elements instanceof Element) return [elements];
         throw new TypeError("Invalid target to initialize chart on.");
-    }
-
-    /**
-     * 
-     */
-    private static parseGraphData(data: Object): Object {
-        return {
-
-        };
     }
 }
